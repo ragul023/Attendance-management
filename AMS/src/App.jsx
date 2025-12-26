989d8043fb852972a461c15fb933f9085602e01f
ragul023 <ragul4172@gmail.com>
2025-12-26 15:51:56 +0530

    Mui buttons

--- AMS/src/App.jsx
+++ AMS/src/App.jsx
@@ -1,13 +1,23 @@
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function App() {
-
   return (
-    <>
-      <div>
-         <h1>Attendance Management system</h1>
-       </div>
-    </>
-  )
+    <div style={{ padding: 20 }}>
+      <Button variant="contained" color="primary">
+        Hello MUI
+      </Button>
+
+      <Button
+        variant="outlined"
+        color="error"
+        startIcon={<DeleteIcon />}
+        style={{ marginLeft: 10 }}
+      >
+        Delete
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
